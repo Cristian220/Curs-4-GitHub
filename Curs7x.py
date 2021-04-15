8301c828db5872eb733600cc543adcb9cb90512a
Costinas <gabriel_cris2002@yahoo.com>
2021-04-15 17:52:40 +0300

    Main menu config and empty user function

--- Curs7x.py
+++ Curs7x.py
@@ -1,1 +1,35 @@
-print("Hello Worldd")
+def add_Book():
+    print("Add a book option")
+def list_Books():
+    print("List books option")
+def update_Book():
+    print("Update book option")
+def share_Book():
+    print("Share book option")
+
+
+
+
+
+
+
+
+#Main menu for use
+print("Menu : ")
+print("1 : Add a book")
+print("2 : List books")
+print("3 : Update book")
+print("4 : Share book")
+option = input("Select one option -> ")
+if option == 1:
+    add_Book()
+elif option == 2:
+    list_Books()
+elif option == 3:
+    update_Book()
+elif option == 4:
+    share_Book()
+else:
+    print("Not a valid option")
+
+
